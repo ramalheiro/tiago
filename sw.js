724246075f6d41d523d13bf870301ef00b8213f4
ramalheiro <ramalheiro@me.com>
2026-05-15 18:38:22 -0300

    Add service worker

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'formula5-cache-v2';
+const urlsToCache = ['./', './index.html', './manifest.json', './assets/icon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
